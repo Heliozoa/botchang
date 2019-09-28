5fce1c5af13c0ddd1c41195ad9725f068874783e
Heliozoa <dm89132@gmail.com>
2019-09-28 20:22:27 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#[macro_use]
+extern crate lazy_static;
+
+use regex::Regex;
+use std::collections::HashMap;
+use std::env;
+use twitchchat::commands;
+use twitchchat::*;
+
+const BOT_USERNAME_VAR: &str = "BOT_USERNAME";
+const OAUTH_TOKEN_VAR: &str = "OAUTH_TOKEN";
+const CHANNEL_NAME_VAR: &str = "CHANNEL_NAME";
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"^!(\S+) ?(.*)").unwrap();
+}
+
+fn main() {
+    let vars = env::vars().collect::<HashMap<String, String>>();
+    let bot_username = vars
+        .get(BOT_USERNAME_VAR)
+        .expect("missing BOT_USERNAME environment variable");
+    let oauth_token = vars
+        .get(OAUTH_TOKEN_VAR)
+        .expect("missing OAUTH_TOKEN environment variable");
+    let channel_name = vars
+        .get(CHANNEL_NAME_VAR)
+        .expect("missing CHANNEL_NAME_VAR environment variable");
+
+    let client = twitchchat::connect_easy(bot_username, oauth_token)
+        .expect("failed to connect")
+        .filter::<commands::PrivMsg>();
+
+    let botchang = BotChang {
+        channel: channel_name.to_string(),
+        writer: client.writer(),
+    };
+
+    for event in client {
+        match event {
+            Event::Message(Message::PrivMsg(msg)) => {
+                botchang.handle(msg);
+            }
+            Event::IrcReady(_) | Event::TwitchReady(_) => {
+                println!("ready, joining {}", channel_name);
+                botchang
+                    .writer
+                    .join(channel_name)
+                    .expect("failed to join channel");
+            }
+            Event::Message(_message) => println!("ms {:?}", _message),
+            Event::Error(_error) => println!("er {:?}", _error),
+        }
+    }
+}
+
+struct BotChang {
+    channel: String,
+    writer: Writer,
+}
+
+impl BotChang {
+    fn handle(&self, msg: commands::PrivMsg) {
+        let _user = msg.user();
+        let message = msg.message();
+        let cap = RE.captures(message);
+        if let Some(cap) = cap {
+            let cmd = &cap[1];
+            match cmd {
+                "bap" => {
+                    self.bap();
+                }
+                _ => (),
+            }
+        }
+    }
+
+    fn bap(&self) {
+        self.writer.send(&self.channel, "SachikoPresent").unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(4));
+        self.writer.send(&self.channel, "SachikoBap").unwrap();
+    }
+}
