5ade719928bc96ec5fb0d3ce9ee22a32b684139e
Heliozoa <dm89132@gmail.com>
2020-02-13 16:36:14 +0200

    upgraded twitchchat

--- src/main.rs
+++ src/main.rs
@@ -2,97 +2,90 @@
 extern crate lazy_static;
 
 use dotenv::dotenv;
+use log::{debug, info, trace, warn};
 use regex::Regex;
 use std::collections::HashMap;
 use std::env;
-use twitchchat::commands;
+use tokio::stream::StreamExt;
+use twitchchat::client::Status;
 use twitchchat::*;
-
-const BOT_USERNAME_VAR: &str = "BOT_USERNAME";
-const OAUTH_TOKEN_VAR: &str = "OAUTH_TOKEN";
-const CHANNEL_NAME_VAR: &str = "CHANNEL_NAME";
 
 lazy_static! {
     static ref RE: Regex = Regex::new(r"^!(\S+)(?: (.+))?").unwrap();
 }
 
-fn main() {
-    dotenv().ok();
+#[tokio::main]
+async fn main() {
+    let _ = env_logger::init();
+    let (bot_username, oauth_token, channel_name) = get_env_vars();
+    let client = Client::new();
+    client
+        .writer()
+        .join(&channel_name)
+        .await
+        .expect("failed to join");
+    trace!("joined {}", channel_name);
 
-    let vars = env::vars().collect::<HashMap<String, String>>();
+    {
+        let client = client.clone();
+        tokio::task::spawn(async move {
+            trace!("running handler");
+            let mut privmsgs = client.dispatcher().await.subscribe::<events::Privmsg>();
+            trace!("subscribed");
+            while let Some(msg) = privmsgs.next().await {
+                debug!("[{}] {}: {}", msg.channel, msg.name, msg.data);
+                process(&client, &*msg).await;
+            }
+        });
+    }
+
+    let (read, write) = twitchchat::connect_easy(&bot_username, &oauth_token, Secure::UseTls)
+        .await
+        .expect("failed to connect");
+    let rate_limit = rate_limit::RateLimit::full(20, std::time::Duration::from_secs(30));
+    match client
+        .run_with_user_rate_limit(read, write, rate_limit)
+        .await
+    {
+        Ok(Status::Eof) => println!("EOF"),
+        Ok(Status::Canceled) => println!("canceled"),
+        Err(err) => eprintln!("error!: {}", err),
+    };
+}
+
+fn get_env_vars() -> (String, String, String) {
+    dotenv().ok();
+    let mut vars = env::vars().collect::<HashMap<String, String>>();
     let bot_username = vars
-        .get(BOT_USERNAME_VAR)
+        .remove("BOT_USERNAME")
         .expect("missing BOT_USERNAME environment variable");
     let oauth_token = vars
-        .get(OAUTH_TOKEN_VAR)
+        .remove("OAUTH_TOKEN")
         .expect("missing OAUTH_TOKEN environment variable");
     let channel_name = vars
-        .get(CHANNEL_NAME_VAR)
+        .remove("CHANNEL_NAME")
         .expect("missing CHANNEL_NAME_VAR environment variable");
-
-    let client = twitchchat::connect_easy(bot_username, oauth_token)
-        .expect("failed to connect")
-        .filter::<commands::PrivMsg>();
-
-    let botchang = BotChang {
-        channel: channel_name.to_string(),
-        writer: client.writer(),
-    };
-
-    for event in client {
-        match event {
-            Event::Message(Message::PrivMsg(msg)) => {
-                botchang.handle(msg);
-            }
-            Event::IrcReady(_) | Event::TwitchReady(_) => {
-                println!("ready, joining {}", channel_name);
-                botchang
-                    .writer
-                    .join(channel_name)
-                    .expect("failed to join channel");
-            }
-            Event::Message(_message) => println!("ms {:?}", _message),
-            Event::Error(_error) => println!("er {:?}", _error),
-        }
-    }
+    (bot_username, oauth_token, channel_name)
 }
 
-struct BotChang {
-    channel: String,
-    writer: Writer,
-}
-
-impl BotChang {
-    fn handle(&self, msg: commands::PrivMsg) {
-        let user = msg.user();
-        let message = msg.message();
-        let cap = RE.captures(message);
-        if let Some(cap) = cap {
-            let cmd = &cap[1];
-            let args = &cap
-                .get(2)
-                .map(|args| args.as_str().split(" ").collect::<Vec<_>>())
-                .unwrap_or(vec![]);
-
-            match (cmd, args.as_slice()) {
-                (_, _) => self.command(&cmd, &args),
+async fn process(client: &Client, msg: &messages::Privmsg<'_>) {
+    if let Some(caps) = RE.captures(&msg.data) {
+        let command = &caps[1];
+        let args = &caps
+            .get(2)
+            .map_or(vec![], |m| m.as_str().split_whitespace().collect());
+        info!("{}: {}({:?})", msg.name, command, args);
+        match (command, args.as_slice()) {
+            ("test", []) => {
+                client
+                    .writer()
+                    .privmsg(&msg.channel, "test")
+                    .await
+                    .expect("failed to send message");
             }
-        } else {
-            self.echo(&user, &message);
+            _ => {
+                warn!("no command matched");
+            }
         }
-    }
-
-    fn command(&self, cmd: &str, args: &[&str]) {
-        let msg = format!("command {} with {:?}", cmd, args);
-        self.send(&msg);
-    }
-
-    fn echo(&self, user: &str, msg: &str) {
-        let msg = format!("echo {}: {}", user, msg);
-        self.send(&msg);
-    }
-
-    fn send(&self, msg: &str) {
-        self.writer.send(&self.channel, msg).unwrap();
     }
 }
